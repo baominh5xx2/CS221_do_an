5c64a9da193744c448fe4431d3eccb91222dc991
Bao Nguyen <baonm@metadatasolutions.vn>
2025-12-13 01:56:26 +0700

    refactor code

--- src/train_t5.py
+++ src/train_t5.py
@@ -329,8 +329,6 @@ def main():
         save_strategy="epoch",
         eval_strategy="epoch",
         load_best_model_at_end=True,
-        metric_for_best_model="f1_macro",  # Use F1 macro as main metric
-        greater_is_better=True,  # Higher F1 is better
         save_total_limit=1,
         do_train=True,
         do_eval=True,
